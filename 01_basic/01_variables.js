34077eb6d38ce39f6b1e3b18756eb2222f63181f
Muhammad Abu Bakar <154862242+abubakar1010@users.noreply.github.com>
2024-01-23 18:01:10 +0000

    finished concept of let, var and const

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 23447
+
+let city = "dhaka"
+
+var time = "3497"
+
+// accountId = 34798  //not alowed use multiple constant
+
+time = 73782
+
+city = "barishal"
+
+let account;
+
+console.table([accountId, city, time, account])
+
+/* prefer not to use var 
+because of issue in block scope and functional scope */
