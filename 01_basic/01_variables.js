c9cb6c6161573d4da4bb59c248e58142a3378a33
Muhammad Abu Bakar <154862242+abubakar1010@users.noreply.github.com>
2024-01-23 18:04:36 +0000

    finished let, var and const's concept

--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -12,7 +12,10 @@ city = "barishal"
 
 let account;
 
+// console.log(accountId);
+
 console.table([accountId, city, time, account])
 
+// multiline Comment example goes here 
 /* prefer not to use var 
 because of issue in block scope and functional scope */
