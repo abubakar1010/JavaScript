c4a1876c68221b0ea064c51f0ce8f252c35f6cf4
Muhammad Abu Bakar <154862242+abubakar1010@users.noreply.github.com>
2024-01-23 18:34:15 +0000

    finished the concept of datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,37 @@
+"use strict"  //treat all js code as new vertion
+
+// alert ( 3 + 3)  we are using nodejs, not browser
+
+let name = "Muhammad"
+
+let age = 19
+
+let isLogedIn = true 
+
+let state;
+
+// number => 2 to power 53 
+
+// Bigint
+
+//string => ""
+
+//Boolean => true/false
+
+//nul => standalone value
+
+//undefined =>
+
+//symbol => unique
+
+//object
+
+console.log(typeof null); // the output is object
+
+console.log(typeof name); // the output is string
+
+console.log(typeof age); // the output is number
+
+console.log(typeof state); // the output is undefined
+
+console.log(typeof isLogedIn); // the output is boolean
