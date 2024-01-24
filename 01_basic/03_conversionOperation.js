21794c97fa25612291005d3f2eaf2a73415dc8e3
Abubakar1010 <abubakar850772@gmail.com>
2024-01-24 21:55:36 +0600

    operation of js

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -4,37 +4,84 @@ let stringInNumber = Number(name);
 
 // console.log(typeof(name));
 
-console.log(typeof stringInNumber);
+//console.log(typeof stringInNumber);
 
 //number
 
-console.log(stringInNumber);
+//console.log(stringInNumber);
 
 //NaN (not an number)
 
 let stringInBoolean = Boolean(name);
 
-console.log(typeof stringInBoolean);
+//console.log(typeof stringInBoolean);
 
 //boolean
 
-console.log(stringInBoolean);
+//console.log(stringInBoolean);
 
 //true
 
 let num = null;
 let nullInNumber = Number(num);
 
-console.log(nullInNumber);
+// console.log(nullInNumber);
 
-console.log(typeof nullInNumber);
+// console.log(typeof nullInNumber);
 
 
 let someNumber = 66
 
 let numberInString = String(someNumber)
 
-console.log(typeof numberInString)
-console.log(numberInString)
+// console.log(numberInString)
+
+//console.log(typeof numberInString)
+//console.log(numberInString)
+
+// ------------------------ some notes ---------------------------------
+
+// "" => undefined
+
+// "66" => number
+
+// "776asb" => NaN (not an number)
+
+//nul => number (value=0)
+
+//1 => true
+
+
+
+// **************************** operation ******************************
+
+
+
+
+let addition = 5 + 5 
+let subtraction= 5 - 5 
+let divide= 5 / 5 
+let multiplication = 5 * 5 
+let reminder = 7 % 8
+
+// console.log(city)
+
+let first = "Abu"
+
+let second = " Bakar"
+
+let final = first + second
+
+// console.log(final)
+
+// console.log("abu" + " bakar");
+
+// console.log(( "5" + "7"));
+
+// console.log( 5 + "7");
+
+// console.log(5 + 7 + "9");
+
+// console.log( "9" + 5 + 7)
 
 
