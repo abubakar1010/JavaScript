e22b2a307c6ca86ce654a387a74ee927843f115a
Abubakar1010 <abubakar850772@gmail.com>
2024-01-24 22:24:24 +0600

    finished comparison of js

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,30 @@
+
+// console.log( 9 < 8);
+
+// console.log( 9 == 9);
+
+// console.log( 5 === 5);
+
+// console.log (7 == 6);
+
+// console.log( 7 == "7");
+
+// console.log ( 7 === "7");
+
+// console.log( "7" === "7");
+
+
+// console.log( null == 0)
+// console.log( null <= 0)
+// console.log( null === 0)
+// console.log( null >= 0)
+
+
+// console.log(undefined <= 0)
+// console.log(undefined >= 0)
+// console.log(undefined == 0)
+// console.log(undefined === 0)
+
+// console.log( true == 0)
+// console.log( true == 1)
+// console.log( 1 === false)
