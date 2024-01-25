e7aa03040558e044c0083df2a9bb2ef3283827bc
Abubakar1010 <abubakar850772@gmail.com>
2024-01-25 15:43:56 +0600

    complete math of js

---
+++ 01_basic/06_number_and_maths.js
@@ -0,0 +1,44 @@
+let score = 67;
+
+// console.log(score);
+// console.log(typeof score);
+
+let balance = new Number(836);
+
+// console.log(balance);
+// console.log(typeof balance);
+
+let number = 8745.8676;
+
+// console.log(number.toPrecision(3));
+
+let anotherNumber = 48792;
+
+// console.log(anotherNumber.toString().charAt(3))
+// console.log(anotherNumber.toString().indexOf('8'))
+
+let thousand = 1000000;
+
+// console.log(thousand.toLocaleString("en-BN"));
+
+// ------------------- Maths ------------------------
+
+// console.log(Math.abs(-39));
+// console.log(Math.ceil(87.00002));
+// console.log(Math.floor(77.99));
+// console.log(Math.max(57, 84, 648,48));
+// console.log(Math.min(57, 84, 648,48));
+
+// console.log(Math.random());
+
+// console.log(Math.random() * 10);
+
+// console.log((Math.random() * 10) + 1);
+
+// console.log(Math.floor((Math.random() * 10) + 1));
+
+const max = 30;
+
+const min = 10;
+
+console.log(Math.floor(Math.random() * ((max -min) + 1) + min))
